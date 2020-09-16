a9afccbec1ad60a96df084f43704cbe5af6c5b4c
zarienalee <zariena5@gmail.com>
2020-09-16 18:06:00 +0200

    person project

---
+++ Desktop/Person/person.js
@@ -0,0 +1,13 @@
+let person = {
+    name: 'Ryan',
+    age: '30',
+    gender: 'female',
+    interests: ['being a hardarse','agile', 'ssd hard drives']
+}   
+
+let person = newPerson('Ryan',30,'male',['being a hardarse','agile', 'ssd hard drives'] )
+let greeting = person.hello()
+console.log(greeting);   
+
+
+ 
